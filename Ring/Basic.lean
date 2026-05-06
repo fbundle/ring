9838b248dd15f852f0c0f3f38389a19f6fa992f6
khanh <khanh.nguyen.contact@gmail.com>
2026-05-07 02:20:09 +0800

    auto_commit

--- Ring/Basic.lean
+++ Ring/Basic.lean
@@ -3,10 +3,10 @@ import Mathlib.Algebra.Group.Defs
 import Mathlib.Algebra.Group.Subgroup.Defs
 -- import Mathlib.Algebra.Ring.Basic
 
-namespace Ring
+namespace CommRing
 
 
-class CommRing α extends AddCommGroup α where
+class Ring α extends AddCommGroup α where
   one: α
   mul: α → α → α
   mul_one (a: α): mul a one = a
@@ -16,27 +16,27 @@ class CommRing α extends AddCommGroup α where
   right_distrib (a b c : α) : mul (a + b) c = mul a c + mul b c
 
 
-instance [CommRing α]: One α where
-  one := CommRing.one
+instance [Ring α]: One α where
+  one := Ring.one
 
-instance [CommRing α]: Mul α where
-  mul := CommRing.mul
+instance [Ring α]: Mul α where
+  mul := Ring.mul
 
-class CommRingHom α β [CommRing α] [CommRing β] where
+class Hom α β [Ring α] [Ring β] where
   map_func: α → β
   map_add (a b: α): map_func (a + b) = map_func a + map_func b
   map_mul (a b: α): map_func (a * b) = map_func a * map_func b
   map_zero: map_func 0 = 0
   map_one: map_func 1 = 1
 
-class Ideal α [CommRing α] extends AddSubgroup α where
+class Ideal α [Ring α] extends AddSubgroup α where
   mul_closed (a b: α): a ∈ carrier → a * b ∈ carrier
 
 
 
-def zero_divisor [CommRing α] (x: α): Prop := ∃ (y: α), x * y = 0
+def zero_divisor [Ring α] (x: α): Prop := ∃ (y: α), x * y = 0
 
-def unit [CommRing α] (x: α): Prop := ∃ (y: α), x * y = 1
+def unit [Ring α] (x: α): Prop := ∃ (y: α), x * y = 1
 
 
 
