43c2882ab74fdcf4e180161f578e8944e0d7cff5
khanh <khanh.nguyen.contact@gmail.com>
2026-05-07 02:14:13 +0800

    auto_commit

--- Ring/Basic.lean
+++ Ring/Basic.lean
@@ -1,4 +1,43 @@
+import Mathlib.Data.Set.Defs
+import Mathlib.Algebra.Group.Defs
+-- import Mathlib.Algebra.Ring.Basic
+
 namespace Ring
+
+
+class CommRing α extends AddCommGroup α where
+  one: α
+  mul: α → α → α
+  mul_one (a: α): mul a one = a
+  mul_comm (a b: α): mul a b = mul b a
+  mul_assoc (a b c: α): mul (mul a b) c = mul a (mul b c)
+  left_distrib (a b c : α) : mul a (b + c) = mul a b + mul a c
+  right_distrib (a b c : α) : mul (a + b) c = mul a c + mul b c
+
+
+instance [CommRing α]: One α where
+  one := CommRing.one
+
+instance [CommRing α]: Mul α where
+  mul := CommRing.mul
+
+class CommRingHom α β [CommRing α] [CommRing β] where
+  map_func: α → β
+  map_add (a b: α): map_func (a + b) = map_func a + map_func b
+  map_mul (a b: α): map_func (a * b) = map_func a * map_func b
+  map_zero: map_func 0 = 0
+  map_one: map_func 1 = 1
+
+
+
+
+def zero_divisor [CommRing α] (x: α): Prop := ∃ (y: α), x * y = 0
+
+def unit [CommRing α] (x: α): Prop := ∃ (y: α), x * y = 1
+
+
+
+
 
 
 
